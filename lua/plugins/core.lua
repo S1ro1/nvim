455e055df1a72b10ac7a0d563881c4119d100692
S1ro1 <matej.sirovatka@gmail.com>
2023-11-21 15:23:46 +0100

    Change to copilot.lua, minor changes

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,12 +1,11 @@
 return {
-  -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
+  { "folke/tokyonight.nvim" },
 
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "tokyonight",
     },
   },
 }
